a3581e69128cb6fb93be3a7050d3255821c3533e
benmelz <ben@melz.me>
2022-04-17 22:14:01 -0400

    feat: mvp

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './index.css';
+
+window.addEventListener('load', () => {
+  let hue = 0;
+  const randomizeHue = () => hue = (hue + (Math.random() * 358) + 1) % 360;
+  const renderHue = () => document.documentElement.setAttribute('style', `background-color: hsl(${hue}, 18%, 43%);`);
+  const redraw = () => [randomizeHue, renderHue].forEach((func) => func());
+  document.addEventListener('click', redraw);
+  document.addEventListener('keydown', redraw);
+});
