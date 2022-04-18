7763028d96d14bf2e0e6575bebc04b82cc16cdc1
benmelz <ben@melz.me>
2022-04-17 22:24:45 -0400

    feat: limit hue jumps to multiples of 10

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './index.css';
 
 window.addEventListener('load', () => {
   let hue = 0;
-  const randomizeHue = () => hue = (hue + (Math.random() * 358) + 1) % 360;
+  const randomizeHue = () => hue = (hue + ((Math.random() * 34) + 1) * 10) % 360;
   const renderHue = () => document.documentElement.setAttribute('style', `background-color: hsl(${hue}, 18%, 43%);`);
   const redraw = () => [randomizeHue, renderHue].forEach((func) => func());
   document.addEventListener('click', redraw);
